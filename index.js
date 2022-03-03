fd71e07bf2c7f8122eea7661d7520d9e6718f67b
Colton Garcia <coltontgarcia@gmail.com>
2022-03-03 10:03:25 -0700

    this is my initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+document.querySelector("form").addEventListener("submit", addMovie);
+
+const message = document.querySelector('#message')
+
+function addMovie(event) {
+  event.preventDefault();
+
+  let inputField = document.querySelector('input')
+
+  const movie = document.createElement('li')
+
+  const movieTitle = document.createElement("span");
+
+  movieTitle.textContent = inputField.value;
+
+  movieTitle.addEventListener("click", crossOffMovie);
+
+  movie.appendChild(movieTitle)
+
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.textContent = "x";
+
+  deleteBtn.addEventListener("click", deleteMovie);
+
+  movie.appendChild(deleteBtn);
+
+  const list = document.querySelector("ul");
+
+  list.appendChild(movie);
+
+  inputField.value = ''
+}
